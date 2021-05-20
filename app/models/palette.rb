7e54bf579100dda4b95b30ec77128428928f4e0c
stephenavocado <stephenwalter@chicagobooth.edu>
2021-05-20 04:26:40 +0000

    user only sees their projects now

--- app/models/palette.rb
+++ app/models/palette.rb
@@ -14,5 +14,5 @@
 class Palette < ApplicationRecord
   belongs_to(:user, { :required => false, :class_name => "User", :foreign_key => "user_id" })
   # , :counter_cache => :pallets_count
-  has_many(:saves, { :class_name => "Safe", :foreign_key => "palette_id", :dependent => :nullify })
+  # has_many(:saves, { :class_name => "Safe", :foreign_key => "palette_id", :dependent => :nullify })
 end
