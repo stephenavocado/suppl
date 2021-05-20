bda75f9bfc7ac775c56225663ab21fdba0aa1ee0
stephenavocado <stephenwalter@chicagobooth.edu>
2021-05-20 02:41:23 +0000

    Fixed unitialized constant : Rooms

--- app/models/room.rb
+++ app/models/room.rb
@@ -11,4 +11,7 @@
 #  project_id     :integer
 #
 class Room < ApplicationRecord
+  belongs_to(:project, { :required => false, :class_name => "Project", :foreign_key => "project_id" })
+  # , :counter_cache => :spaces_count
+  # has_many(:elements, { :class_name => "Element", :foreign_key => "room_id", :dependent => :destroy })
 end
