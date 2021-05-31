f1c4edbee74f462b10cfeb12cf7d02c93e886925
stephenavocado <stephenwalter@chicagobooth.edu>
2021-05-31 15:45:16 +0000

    added accept & reject invite actions

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -32,20 +32,50 @@ class InvitationsController < ApplicationController
     end
   end
 
+  def accept_invite
+    the_id = params.fetch("path_id")
+    the_invitation = Invitation.where({ :id => the_id }).at(0)
+    the_invitation.status = "Accepted"
+
+    if the_invitation.valid?
+      the_invitation.save
+
+      redirect_to("/user/#{session.fetch(:user_id)}", { :notice => "Invitation accepted."} )
+    else
+      redirect_to("/user/#{session.fetch(:user_id)", { :alert => "Invitation failed to update successfully." })
+    end
+  end
+
+  def decline_invite
+    the_id = params.fetch("path_id")
+    the_invitation = Invitation.where({ :id => the_id }).at(0)
+    the_invitation.status = "Declined"
+
+    if the_invitation.valid?
+      the_invitation.save
+
+      redirect_to("/user/#{session.fetch(:user_id)}", { :notice => "Invitation declined."} )
+    else
+      redirect_to("/user/#{session.fetch(:user_id)", { :alert => "Invitation failed to update successfully." })
+    end
+  end
+
   def update
     the_id = params.fetch("path_id")
     the_invitation = Invitation.where({ :id => the_id }).at(0)
 
-    the_invitation.recipient_id = params.fetch("query_recipient_id")
-    the_invitation.project_id = params.fetch("query_project_id")
-    the_invitation.sender_id = params.fetch("query_sender_id")
+    #the_invitation.recipient_id = params.fetch("query_recipient_id")
+    #the_invitation.project_id = params.fetch("query_project_id")
+    #the_invitation.sender_id = params.fetch("query_sender_id")
     the_invitation.status = params.fetch("query_status")
 
     if the_invitation.valid?
       the_invitation.save
-      redirect_to("/invitations/#{the_invitation.id}", { :notice => "Invitation updated successfully."} )
+
+      redirect_to("/user/#{session.fetch(:user_id)}", { :notice => "Invitation accepted."} )
+
     else
-      redirect_to("/invitations/#{the_invitation.id}", { :alert => "Invitation failed to update successfully." })
+      redirect_to("/user/#{session.fetch(:user_id)", { :alert => "Invitation failed to update successfully." })
     end
   end
 
