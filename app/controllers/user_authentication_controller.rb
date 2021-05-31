a4f86005d769518f9d39ba81cb5bf775c82ad8a1
stephenavocado <stephenwalter@chicagobooth.edu>
2021-05-31 14:01:46 +0000

    invitation record shows up on user show page

--- app/controllers/user_authentication_controller.rb
+++ app/controllers/user_authentication_controller.rb
@@ -2,6 +2,14 @@ class UserAuthenticationController < ApplicationController
   # Uncomment this if you want to force users to sign in before any other actions
   # skip_before_action(:force_user_sign_in, { :only => [:sign_up_form, :create, :sign_in_form, :create_cookie] })
 
+  def show
+
+    matching_invitations = Invitation.all
+    @list_of_invitations = matching_invitations.order({ :created_at => :desc })
+    
+    render({ :template => "user_authentication/show.html.erb" })
+  end
+  
   def sign_in_form
     render({ :template => "user_authentication/sign_in.html.erb" })
   end
